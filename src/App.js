de1e62cbf31d2a407909bdf7c9b61bae0103fa21
VISHNU E <vishnuelavarasan2002@gmail.com>
2025-10-13 18:20:07 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import About from './components/About';
+import Projects from './components/Projects';
+import Resume from './components/Resume';
+import Contect from './components/Contect';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Hero/>
+      <About/>
+      <Projects/>
+      <Resume/>
+      <Contect/>
+      <Footer/>
+      
+    </div>
+  );
+}
+
+export default App;
+ 
